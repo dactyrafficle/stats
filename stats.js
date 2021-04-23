2f8c3c8fbf8a70e9b64048d433725e085b0153f7
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-04-23 19:08:32 -0400

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,17 +1,4 @@
-function rnorm() {
-  var n = 1000;
-  var x = 0;
-  var ss = 0;
-  for (var i = 0; i < n; i++) {
-    var a = Math.random();
-    x += a;
-    ss += Math.pow(a-0.5, 2);
-  }
-  var xbar = x/n;
-  var v = ss/n;
-  var sd = Math.sqrt(v);  
-  return (xbar-0.5)/(sd/Math.sqrt(n));
-};
+
 function chisq(df) {
   if (arguments.length === 0) {
     df = 1;
